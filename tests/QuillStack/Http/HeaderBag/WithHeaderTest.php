5f37c4b7938e3861cb2da27d874c136af9ef1178
Radek Ziemniewicz <radoslaw@ziemniewicz.pl>
2020-09-27 15:35:57 +0100

    Added happy path tests for WithHeader method

---
+++ tests/QuillStack/Http/HeaderBag/WithHeaderTest.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+
+namespace QuillStack\Http\HeaderBag;
+
+use PHPUnit\Framework\TestCase;
+use QuillStack\Mocks\HeaderBag\SimpleHeaders;
+
+final class WithHeaderTest extends TestCase
+{
+    private const NEW_HEADER = 'new-header';
+
+    private const EXISTING_HEADER = ':path';
+
+    /**
+     * @var HeaderBag
+     */
+    private HeaderBag $bag;
+
+    protected function setUp(): void
+    {
+        $headers = (new SimpleHeaders())->headers;
+        $this->bag = new HeaderBag($headers);
+    }
+
+    public function testWithNewHeader()
+    {
+        $bag = $this->bag->withHeader(self::NEW_HEADER, 'test');
+
+        $this->assertCount(16, $this->bag->getHeaders());
+        $this->assertCount(17, $bag->getHeaders());
+
+        $this->assertFalse($this->bag->hasHeader(self::NEW_HEADER));
+        $this->assertTrue($bag->hasHeader(self::NEW_HEADER));
+
+        $this->assertEquals('', $this->bag->getHeaderLine(self::NEW_HEADER));
+        $this->assertEquals('test', $bag->getHeaderLine(self::NEW_HEADER));
+    }
+
+    public function testWithExistingHeader()
+    {
+        $bag = $this->bag->withHeader(self::EXISTING_HEADER, '/test');
+
+        $this->assertCount(16, $this->bag->getHeaders());
+        $this->assertCount(16, $bag->getHeaders());
+
+        $this->assertTrue($this->bag->hasHeader(self::EXISTING_HEADER));
+        $this->assertTrue($bag->hasHeader(self::EXISTING_HEADER));
+
+        $this->assertEquals('/', $this->bag->getHeaderLine(self::EXISTING_HEADER));
+        $this->assertEquals('/test', $bag->getHeaderLine(self::EXISTING_HEADER));
+    }
+}
